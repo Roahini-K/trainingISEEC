7238f4bb3bc697dcbc81f815da4e067e4c0766b7
Roahini-K <86963628+Roahini-K@users.noreply.github.com>
2021-12-02 12:18:52 +0530

    Create task4_salary.java

---
+++ taskAssignment/task4_salary.java
@@ -0,0 +1,25 @@
+package taskTraining;
+
+import java.util.Scanner;
+
+public class task4_salary {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the total Salary paid: ");
+	    int salary = sc.nextInt();
+	    int weekend=(salary-800)/130;
+	    int weekdays=10+weekend;
+	    System.out.println("Number of weekdays hours: "+weekdays);
+	    System.out.println("Number of weekend hours: "+weekend);
+	    
+	    
+	    
+	   
+		
+	}
+	
+    
+
+	   
+	}
+
