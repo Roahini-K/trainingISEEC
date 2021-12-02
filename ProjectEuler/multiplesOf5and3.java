9e9063185c92ef17b573d400791a841000908541
Roahini-K <86963628+Roahini-K@users.noreply.github.com>
2021-12-02 10:40:20 +0530

    Create multiplesOf5and3.java

---
+++ ProjectEuler/multiplesOf5and3.java
@@ -0,0 +1,14 @@
+package projEuler;
+
+public class multiplesof5and3 {
+	static int sum=0;
+	public static void main(String args[]) {
+	for(int i=1;i<1000;i++) {
+	if(i%3==0||i%5==0) {
+	sum=sum+i;
+	}
+	}
+	System.out.println(sum);
+	}
+
+}
